98be9e4d2629b93dd2b47a37d031b121bd874af2
Hoa Nguyen <hoanguyen.rin@gmail.com>
2024-11-27 12:38:21 +0700

    feat: add mainnet for testing

--- projects/ngx-wagmi-example/src/app/app.config.ts
+++ projects/ngx-wagmi-example/src/app/app.config.ts
@@ -4,15 +4,16 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { provideTanStackQuery, QueryClient, withDevtools } from '@tanstack/angular-query-experimental';
 import { createConfig, http } from '@wagmi/core';
-import { arbitrumSepolia, baseSepolia } from '@wagmi/core/chains';
+import { arbitrumSepolia, baseSepolia, mainnet } from '@wagmi/core/chains';
 import { provideHighlightOptions } from 'ngx-highlightjs';
 import { provideConfig } from 'ngx-wagmi';
 import { routes } from './app.routes';
 
 const defaultConfig = createConfig({
-  chains: [baseSepolia, arbitrumSepolia],
+  chains: [baseSepolia, mainnet, arbitrumSepolia],
   transports: {
     [baseSepolia.id]: http(),
+    [mainnet.id]: http(),
     [arbitrumSepolia.id]: http(),
   },
 });
